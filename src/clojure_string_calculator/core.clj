5bfc8084836a2a443355ea8b761e9ff9c680f9cb
calebphillips <caleb.phillips@gmail.com>
2011-01-06 20:43:05 -0600

    extracted the add-numbers method after the 2 numbers examples

--- src/clojure_string_calculator/core.clj
+++ src/clojure_string_calculator/core.clj
@@ -1,6 +1,8 @@
 (ns clojure-string-calculator.core
   (:use clojure.contrib.str-utils))
 
+(defn add-numbers [s]
+  (reduce + (map #(Integer. %) (re-split #"," s))))
+
 (defn add [s] 
-  (if (empty? s) 0
-    (reduce + (map #(Integer. %) (re-split #"," s)))))
+  (if (empty? s) 0 (add-numbers s)))
