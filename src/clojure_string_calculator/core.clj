d50c2b66cd1d69d687f09f27cdee2fb649624241
calebphillips <caleb.phillips@gmail.com>
2011-01-07 16:12:35 -0600

    refactoring of passing newline tests

--- src/clojure_string_calculator/core.clj
+++ src/clojure_string_calculator/core.clj
@@ -1,11 +1,13 @@
 (ns clojure-string-calculator.core
   (:use clojure.contrib.str-utils))
 
+(defn parse-numbers [s]
+  (map #(Integer. %) (re-split #"," (re-gsub #"\n" "," s))))
+
 (defn add-numbers [s]
-  (reduce + (map #(Integer. %) (re-split #"," s))))
+  (reduce + (parse-numbers s)))
 
 (defn add [s] 
   (if (empty? s) 0
-    (if (.contains s "\n")
-      (add-numbers (re-gsub #"\n" "," s))
-      (add-numbers s))))
+    (add-numbers s)))
+      
