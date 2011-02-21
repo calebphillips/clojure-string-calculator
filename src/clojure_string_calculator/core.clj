5cf3100525a7a1eb767c7722d6f6ad5c3da86f73
calebphillips <caleb.phillips@gmail.com>
2011-02-21 08:31:22 -0600

    Update

--- src/clojure_string_calculator/core.clj
+++ src/clojure_string_calculator/core.clj
@@ -1,28 +1,10 @@
 (ns clojure-string-calculator.core
   (:use 
-    [clojure.contrib.str-utils]
     [clojure.string]))
 
-(defn to-i [s]
-  (try
-    (Integer. s)
-    (catch NumberFormatException e 0)))
-
-(defn replace-custom [s]
-  (if (.startsWith s "//")
-    (replace s (str (get s 2)) ",")
-    s))
-
-(defn normalize-delims [s]
-  (replace (replace-custom s) "\n" ","))
-
-(defn parse-numbers [s]
-  (map to-i (split (normalize-delims s) (re-pattern ","))))
-
-(defn add-numbers [s]
-  (reduce + (parse-numbers s)))
 
 (defn add [s] 
-  (if (empty? s) 0 
-    (add-numbers s)))
+  (if (empty? s) 
+    0
+    (reduce + (map #(Integer. %) (split s #"[,|\n]")))))
       
