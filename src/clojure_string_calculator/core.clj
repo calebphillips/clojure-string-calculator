862f0490346019e17f896331848432ba2af5ff34
calebphillips <caleb.phillips@gmail.com>
2011-02-21 23:05:01 -0600

    factored out the format-exception function

--- src/clojure_string_calculator/core.clj
+++ src/clojure_string_calculator/core.clj
@@ -1,13 +1,16 @@
 (ns clojure-string-calculator.core
-  (:use [clojure.string :only [split]]))
+  (:use [clojure.string :only [split join]]))
 
 (defn to-numbers [s]
       (map #(Integer. %) (filter #(not (empty? %)) (split s #"[^\d|^-]"))))
+
+(defn format-exception [coll]
+      (Exception. (str "Negatives not allowed: " (join ", " coll))))
 
 (defn add [s] 
       (let [numbers (to-numbers s)
             negatives (filter neg? numbers)]
         (if (not-empty negatives)
-          (throw (Exception. (apply str "Negatives not allowed: " (interpose ", " negatives))))
+          (throw (format-exception negatives))
           (reduce + numbers))))
       
