089efba9c50e2b0d9e48e6e85712875fae5edcb6
calebphillips <caleb.phillips@gmail.com>
2011-04-05 15:44:10 -0500

    Removed test/ directory

--- test/clojure_string_calculator/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clojure-string-calculator.test.core
-  (:use [clojure-string-calculator.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
