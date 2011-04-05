461daf5f37acc5be1f9ecd18fc497b00fd2d6c34
calebphillips <caleb.phillips@gmail.com>
2011-04-05 15:42:01 -0500

    Replaced speclj with clojure.test

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-string-calculator "1.0.0-SNAPSHOT"
   :description "String calculator as described here: http://osherove.com/tdd-kata-1/"
   :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[speclj "1.3.1"]]
-  :test-path "spec/")
+  :dev-dependencies []
+  )
--- test/clojure_string_calculator/test/core.clj
+++ test/clojure_string_calculator/test/core.clj
@@ -1,6 +1,31 @@
 (ns clojure-string-calculator.test.core
   (:use [clojure-string-calculator.core] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test]
+        [clojure.string :only [join]]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest string-calculator 
+  (is (= 0 (add "")))
+  
+  (testing "strings with a single number"
+           (is (= 5 (add "5")))
+           (is (= 27 (add "27"))))
+
+  (testing "strings with two numbers"
+           (is (= 3 (add "1,2")))
+           (is (= 8 (add "3,5"))))
+
+  (testing "string with lots of numbers"
+           (is (= 2000 (add (join "," (repeat 100 20))))))
+
+  (testing "the newline as a delimiter"
+           (is (= 6 (add "1\n2\n3")))
+           (is (= 9 (add "3,4\n2"))))
+
+  (testing "custom delimiters"
+           (is (= 15 (add "//;\n9;4;2")))
+           (is (= 13 (add "//x\n2x3x4x4"))))
+
+  (testing "negative numbers"
+           (is (thrown? Exception (add "-1,4,-5")))
+           (is (thrown? Exception "Negatives not allowed: -4, -65" (add "-4,5,-65"))))
+  )
