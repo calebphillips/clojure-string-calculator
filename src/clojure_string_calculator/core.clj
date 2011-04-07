6a55a1a3a195dd0cf2168d8bf7430f8955b90984
calebphillips <caleb.phillips@gmail.com>
2011-04-07 09:28:55 -0500

    Removed guts from the shell practice branch

--- src/clojure_string_calculator/core.clj
+++ src/clojure_string_calculator/core.clj
@@ -1,14 +1,2 @@
 (ns clojure-string-calculator.core
-    (:use [clojure.string :only [join split]]))
-
-(defn to-numbers [s]
-      (map #(Integer. %) (filter not-empty (split s #"[^\d|^-]"))))
-
-(defn format-exception [negatives]
-      (Exception. (str "Negatives not allowed: " (join ", " negatives))))
-
-(defn add [s]
-      (let [numbers (to-numbers s) negatives (filter neg? numbers)]
-        (if (not-empty negatives)
-          (throw (format-exception negatives))
-          (reduce + numbers))))
+    )
